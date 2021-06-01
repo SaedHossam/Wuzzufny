2cfb1289d625c893f6f2eeb247b0eb0f1bbe57c3
ashrafnosser <ashrafnosser66@gmail.com>
2021-06-01 22:16:11 +0200

    PostJob

---
+++ GlassDoor/Controllers/JobsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DAL;
+using DAL.Models;
+using GlassDoor.ViewModels;
+using AutoMapper;
+
+namespace GlassDoor.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public JobsController( IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Jobs
+        [HttpGet]
+        public  ActionResult<IEnumerable<Job>> GetJobs()
+        {
+            return  _unitOfWork.Jobs.GetAll().ToList();
+        }
+
+        // GET: api/Jobs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Job>> GetJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            return job;
+        }
+
+        // PUT: api/Jobs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutJob(int id, Job job)
+        {
+            if (id != job.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(job).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!JobExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Jobs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public  ActionResult<PostJobDto> PostJob([FromBody]PostJobDto postedjob)
+        {
+            if (postedjob == null || !ModelState.IsValid) 
+                return BadRequest();
+
+            var job = _mapper.Map<Job>(postedjob);
+            _unitOfWork.Jobs.Add(job);
+             return Ok(job);
+                    }
+
+        // DELETE: api/Jobs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteJob(int id)
+        {
+            var job = await _context.Jobs.FindAsync(id);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _context.Jobs.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool JobExists(int id)
+        {
+            return _context.Jobs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ GlassDoor/ViewModels/PostJobDto.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GlassDoor.ViewModels
+{
+    public class PostJobDto
+    {
+        [Required]
+        public string Title { get; set; }
+        public string EmploymentType { get; set; }
+        public int? NumberOfVacancies { get; set; }
+        public string Location { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public JobDetailsDto JobDetailsDto { get; set; }
+        public ICollection<SkillsDto> SkillsDto { get; set; }
+
+    }
+}
+
