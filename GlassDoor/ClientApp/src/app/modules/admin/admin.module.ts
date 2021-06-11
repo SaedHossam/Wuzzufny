a6e965ab158e05e5b8399652cdda245c9fc4da10
ahmed samir <ahmmedsamier2017@gmail.com>
2021-06-11 18:13:40 +0200

    added admin side-menu, companyRequests components

--- GlassDoor/ClientApp/src/app/modules/admin/admin.module.ts
+++ GlassDoor/ClientApp/src/app/modules/admin/admin.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { CompanyRequestsComponent } from './components/company-requests/company-requests.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CompanyRequestsComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
---
+++ GlassDoor/ClientApp/src/app/modules/admin/components/company-requests/company-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-requests',
+  templateUrl: './company-requests.component.html',
+  styleUrls: ['./company-requests.component.css']
+})
+export class CompanyRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
