f2f48cddd3e6d30540ebbd79fb378483b0b929e4
SaedHossam <said.hossam12@gmail.com>
2021-06-13 03:40:00 +0200

    finished template for company profile with some design errors, and added it to menu

--- GlassDoor/ClientApp/src/app/modules/company/company-routing.module.ts
+++ GlassDoor/ClientApp/src/app/modules/company/company-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from "./components/home/home.component";
 import { PostJobComponent } from './components/post-job/post-job.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   {path:'postjob',component:PostJobComponent},
+  {path:'profile',component:ProfileComponent},
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   ];
 
